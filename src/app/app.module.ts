442c3f87f403c3c8b1e18dc90bd2d98cb8776f3b
angular7793 <angular7793@caelumBSB01-12>
2018-08-22 21:53:59 -0300

    ex5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FotoModule } from './foto/foto.module';
@@ -20,6 +21,7 @@ import { Erro404Component } from './erro404/erro404.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     FotoModule,
     PainelModule,
     roteamento
--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FotoComponent } from '../foto/foto.component';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
+  foto = new FotoComponent();
+  http: HttpClient;
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    this.http = http;
+    //this.foto.descricao = 'casa';
+    // this.foto.url = 'casa';
+    //this.foto.titulo = 'casa';
+  }
 
   ngOnInit() {
+  }
+
+  salvar(event: Event) {
+    // event.preventDefault();
+    // debugger;
+    let cabecalho = {
+      headers: new HttpHeaders({ 'Content-type': 'application/json' })
+    };
+    this.http.post('http://localhost:3000/v1/fotos', this.foto, cabecalho).subscribe(() => {
+      console.log(`Foto ${this.foto.titulo} Salva`);
+      this.foto = new FotoComponent();
+    },
+      erro => {
+        console.log(erro);
+      });
   }
 
 }
