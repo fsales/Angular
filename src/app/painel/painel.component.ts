dddd93e12f2afbac8ae60c0c40d6f122aa07e9b0
angular7793 <angular7793@caelumBSB01-12>
2018-08-24 22:57:34 -0300

    shadow dom

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,9 +1,15 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
     selector: 'painel',
-    templateUrl: './painel.component.html'
+    templateUrl: './painel.component.html',
+    styleUrls: ['./painel.component.css'],
 })
-export class PainelComponent{
-    @Input() titulo:String;
+export class PainelComponent implements OnInit {
+   
+    @Input() titulo:String = '';
+
+    ngOnInit(): void {
+        this.titulo = this.titulo.length > 7 ? this.titulo.substring(0,7) + '...' : this.titulo;
+    }
 }
